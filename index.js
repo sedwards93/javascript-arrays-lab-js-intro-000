cbb25035d536c68d9741c3804dc833b9e319668e
Symion Edwards <symion.edwards@gmail.com>
2020-03-25 13:16:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,3 +27,9 @@ function prependKitten (name) {
   newArray.unshift(name);
   return newArray
 }
+
+function removeLastKitten () {
+  var newArray = [...kittens];
+  newArray.pop();
+  return newArray
+}
